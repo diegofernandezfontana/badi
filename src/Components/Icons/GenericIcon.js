9bb40c3ed3065cbe835f7a7963a32a5e89242028
Diego Fernandez Fontana <diegofernandezfontana@gmail.com>
2020-01-24 23:37:22 +0100

    Add Generic icon and Icons path's

---
+++ src/Components/Icons/GenericIcon.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { expandMorePath } from "./paths";
+import { palette } from "../../Styles";
+
+const defaultPath = expandMorePath;
+
+const GenericIcon = props => {
+  const { color, scale, path, viewBox, ...restProps } = props;
+
+  const width = viewBox * scale;
+  const height = viewBox * scale;
+  const customViewBox = `0 0 ${viewBox} ${viewBox}`;
+
+  return (
+    <svg width={width} height={height} viewBox={customViewBox} {...restProps}>
+      <path fill={color} d={path} />
+    </svg>
+  );
+};
+
+GenericIcon.defaultProps = {
+  color: palette.grey.dark,
+  path: defaultPath,
+  viewBox: 24,
+  scale: 1
+};
+
+export default GenericIcon;
---
+++ src/Components/Icons/paths/index.js
@@ -0,0 +1,5 @@
+import expandMorePath from "./expandMorePath";
+import searchIconPath from "./searchIconPath";
+import closeIconPath from "./closeIconPath";
+
+export { expandMorePath, searchIconPath, closeIconPath };
